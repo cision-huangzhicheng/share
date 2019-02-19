01b35b52636e85cde0d7da06285eb65770e9240a
cision-huangzhicheng <961644376@qq.com>
2019-02-19 11:18:09 +0800

    python运算符

---
+++ python 运算符.py
@@ -0,0 +1,10 @@
+#！/usr/bin/pyhton3
+a,b = 21,10
+c = 0
+if(a==b):
+  print("a等于b")
+else:
+  print("a不等于b")
+
+
+
---
+++ python赋值运算符.py
@@ -0,0 +1,29 @@
+#！/usr/bin/python3
+
+a,b,c = 21,10,0
+
+c+=a
+print("c+=a :",c)
+
+c =0
+c=+ a
+print("C=+a :",c)
+print('a :',a)
+
+c =25
+c /=a
+print("c/=a :",c)
+
+c=24
+a=21
+c%=a
+print('c%=a :',c)
+
+c=2
+a=4
+c**=a
+print('c**=a',c)
+
+a,c=8,48
+c//=a
+print('c//=a :',c)
